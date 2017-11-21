f343fc68eca3fc43ef70dfad68f706f1400ce319
Christopher <mail@christophermaier.com.au>
2017-11-21 15:14:42 +1000

    added id property to each task. Increments the id for each task

--- todo2.js
+++ todo2.js
@@ -4,9 +4,11 @@ const port = 3000;
 const bodyParser = require('body-Parser');
 
 let todos2 = [
-             { task: 'Pick up car from garage', status: false },
-             { task: 'Buy 4D printer', status: false }
+             { id: 0, task: 'Pick up car from garage', status: false },
+             { id: 1, task: 'Buy 4D printer', status: false }
              ];
+
+let id = 2
 
 // BodyParser to make it easy to read POST data
 app.use(bodyParser.json());
@@ -20,6 +22,7 @@ app.get('/api/todos2', (req, res) => {
 
 // POST /api/todos2
 app.post('/api/todos2', (req, res) => {
+  req.body.id = id++;
   // Add the new task to our array
   todos2.push(req.body);
   // Send the user a list of tasks
