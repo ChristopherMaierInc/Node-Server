7c04b515439d7fb745cb2631da826a6cf66651c1
Christopher <mail@christophermaier.com.au>
2017-11-21 14:31:56 +1000

    User is able to GET / todos2

---
+++ todo2.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-Parser');
+
+let todos2 = [
+             { task: 'Pick up car from garage', status: false },
+             { task: 'Buy 4D printer', status: false }
+             ];
+
+// BodyParser to make it easy to read POST data
+app.use(bodyParser.json());
+
+// GET /students
+app.get('/todos2', (req, res) => {
+  console.log('GET todos2');
+  // Send the user a list of students
+  res.send(todos2);
+})
+
+app.listen(port);
